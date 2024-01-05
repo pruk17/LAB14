e1d00464ca5cd980a2e46bea61670c9815b7481f
Chaiyapruk Parinyanuntakan <chaiyapruk6494@gmail.com>
2024-01-05 16:26:09 +0700

    add lab14-1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME: Chaiyapruk Parinyanunkan
+# STUDENT ID: 660610817
--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,8 +3,25 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	
+	for(int i = 1; i < N; i++){
+		T swap = d[i];
+    	
+		int j = i - 1;
+		while (j >= 0 && d[j] < swap)
+		{
+			d[j+1] = d[j];
+			j--;
+		}
+		d[j+1] = swap;
 
+		cout << "Pass " << i << ":";
+
+		for(int k=0; k < N; k++) cout << d[k] << " ";
+		cout << endl;
+	    }
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -17,3 +34,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
